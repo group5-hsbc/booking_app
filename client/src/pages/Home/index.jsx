4a783e84ca2e7d357016a80393f386864dbb5690
hendryprasetyo <hendryprras@gmail.com>
2023-10-19 06:32:40 +0700

    feat : add herro banner in page home

--- client/src/pages/Home/index.jsx
+++ client/src/pages/Home/index.jsx
@@ -1,10 +1,26 @@
+import test from "../../assets/images/banner-home-1.svg";
 const Home = () => {
-  const BASE_URL_API = import.meta.env.VITE_BASE_URL_API;
   return (
-    <div className="text-red-500">
-      Home
-      <h1>{BASE_URL_API}</h1>
-    </div>
+    <section className="w-full h-max">
+      <div
+        style={{
+          backgroundImage: `url(${test})`,
+          backgroundSize: "cover",
+          backgroundRepeat: "no-repeat",
+          backgroundPosition: "center center",
+        }}
+        className="h-[800px] flex w-full flex-col justify-center items-center gap-8"
+      >
+        <h1 className="text-[3rem] lg:text-[4.5rem] font-bold text-white text-center max-w-[1200px] shadow-sm">
+          Welcome To The Continental Hotel
+        </h1>
+        <div>
+          <button className="px-5 py-2 rounded-full bg-[#148EFF] text-white font-medium shadow-md tracking-wide">
+            Order Here
+          </button>
+        </div>
+      </div>
+    </section>
   );
 };
 
