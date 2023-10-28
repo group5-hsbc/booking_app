7487f269d099aabf3ae0fc4eb404df8acb831c41
Samuelaryo10 <samuelaryo2001@gmail.com>
2023-10-28 19:22:32 +0700

    fit(detail/checkout)

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,8 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { MainLayout } from "./components";
 import { Home } from "./pages";
+import Tentang from "./pages/Tentang";
+import Checkout from "./pages/Checkout";
 
 const App = () => {
   return (
@@ -8,6 +10,8 @@ const App = () => {
       <Routes>
         <Route path="/" element={<MainLayout />}>
           <Route index element={<Home />} />
+          <Route path="/tentang" element={<Tentang />} />
+          <Route path="/checkout" element={<Checkout />} />
         </Route>
       </Routes>
     </Router>
