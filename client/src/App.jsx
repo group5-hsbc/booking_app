2559c0400a6f40d9ce85d8bdddd9bcfcf3d69845
kitsn <kitsnewrachman@gmail.com>
2023-10-28 18:48:26 +0700

    fit {login/regist}

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,8 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { MainLayout } from "./components";
-import { Home } from "./pages";
+import { Home, Hotel } from "./pages";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 
 const App = () => {
   return (
@@ -8,6 +10,9 @@ const App = () => {
       <Routes>
         <Route path="/" element={<MainLayout />}>
           <Route index element={<Home />} />
+          <Route path="/hotel" element={<Hotel />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
         </Route>
       </Routes>
     </Router>
